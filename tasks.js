616563ce276080684206937824807e3906212bb4
ekaspersen <eskilkasp@gmail.com>
2022-01-09 22:41:38 +0100

    everything

---
+++ tasks.js
@@ -0,0 +1,31 @@
+//Task one:
+var firstVar = "";
+
+//Task two:
+var name = "Eskil";
+
+//Task three:
+var number = 60;
+
+//Task four:
+var division = 20/5;
+
+//Task five
+var typeCheck = typeof("frog");
+console.log(typeCheck);
+
+//Task six:
+var orderHasShipped = false;
+
+//Task seven:
+if (orderHasShipped === true) {
+    console.log("true")
+} else {
+    console.log("false")
+}
+
+//task eight:
+var x = 0;
+for (x;x<10;x++) {
+    console.log(x)
+}
